e92b4955dab4391699214fb813ca4f1990363c48
NicoleGaia <nicgaia01@gmail.com>
2025-03-17 10:18:02 -0300

    npm packages

---
+++ npm-packeges-basic/src/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+import logSymbols from 'log-symbols';
+
+console.log(logSymbols.success, chalk.green ("servidor iniciado"))
+console.log(logSymbols.error, "servidor com problema")
