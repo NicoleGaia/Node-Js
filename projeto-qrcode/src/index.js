2f5d39d4743e0f59ea221ec4721cdcf98392b9a3
NicoleGaia <nicgaia01@gmail.com>
2025-03-17 18:26:44 -0300

    create qrcode

--- projeto-qrcode/src/index.js
+++ projeto-qrcode/src/index.js
@@ -1,13 +1,16 @@
 import prompt from "prompt"
+
 import mainPrompt from "./prompts/prompt-main.js"
+import createQRCode from "./services/qr-code/create.js"
+import createPassword from "./services/password/create.js"
 
 async function main() {
-    prompt.get(mainPrompt, function (err, choose){
+    prompt.get(mainPrompt, async function (err, choose){
         if(choose.select == 1){
-            console.log("Escolheu QrCode")
+            await createQRCode()
         }
         if(choose.select == 2){
-            console.log("Escolheu Password")
+            await createPassword()
         }
     })
 
