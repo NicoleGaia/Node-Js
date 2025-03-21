4ad195fd600149a3792a6cb3678aaa18292d6cdd
NicoleGaia <nicgaia01@gmail.com>
2025-03-21 11:58:14 -0300

    chamada para api

--- projeto-app-podcast/src/controllers/podcasts-controller.ts
+++ projeto-app-podcast/src/controllers/podcasts-controller.ts
@@ -1,8 +1,22 @@
 import { IncomingMessage, ServerResponse } from "http"
 
-export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) =>{
+export const getListEpisodes = async (
+    req: IncomingMessage, 
+    res: ServerResponse
+) =>{
     res.writeHead(200,{'content-type': "aplication/json"})
-    res.end({
-       name: "Nicole" 
-    })
+    res.end(JSON.stringify([
+        {
+        podcastName: "flow",
+        episode: "CBUM - Flow #319",
+        videoId: "5DaLlqP-8uI",
+        categories: ["saúde", "esporte", "bodybuilder"]
+        },
+        {
+        podcastName: "flow",
+        episode: "RUBENS BARRICHELLO - Flow #339",
+        videoId: "4KDGTdiOV4I",
+        categories: ["esporte", "corrida"]
+        },
+    ]))
 }
