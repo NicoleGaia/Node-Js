d863b24ecbd040c6165f55bd4c93947d8d3af9ad
NicoleGaia <nicgaia01@gmail.com>
2025-03-21 18:45:48 -0300

    criando rotas

--- projeto-app-podcast/src/controllers/podcasts-controller.ts
+++ projeto-app-podcast/src/controllers/podcasts-controller.ts
@@ -1,6 +1,7 @@
 import { IncomingMessage, ServerResponse } from "http"
 
 import {serviceListEpisodes} from'../services/list-episodes-services'
+import { serviceFilterEpisodes } from "../services/filter-episodes"
 
 export const getListEpisodes = async (
     req: IncomingMessage, 
@@ -9,5 +10,15 @@ export const getListEpisodes = async (
     const content = await serviceListEpisodes()
 
     res.writeHead(200,{'content-type': "aplication/json"})
+    res.end(JSON.stringify(content))
+}
+
+export const getFilterEpisodes = async(
+    req: IncomingMessage, 
+    res: ServerResponse
+) => {
+    const content = await serviceFilterEpisodes("flow")
+
+    res.writeHead(200, { "content-type": "aplication/json"})
     res.end(JSON.stringify(content))
 }
