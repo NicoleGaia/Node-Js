62954e2e674bf0a1fa74b16dc4e914e127d7e495
NicoleGaia <nicgaia01@gmail.com>
2025-03-21 12:06:19 -0300

    create a service

--- projeto-app-podcast/src/controllers/podcasts-controller.ts
+++ projeto-app-podcast/src/controllers/podcasts-controller.ts
@@ -1,22 +1,13 @@
 import { IncomingMessage, ServerResponse } from "http"
 
+import {serviceListEpisodes} from'../services/list-episodes-services'
+
 export const getListEpisodes = async (
     req: IncomingMessage, 
     res: ServerResponse
 ) =>{
+    const content = await serviceListEpisodes()
+
     res.writeHead(200,{'content-type': "aplication/json"})
-    res.end(JSON.stringify([
-        {
-        podcastName: "flow",
-        episode: "CBUM - Flow #319",
-        videoId: "5DaLlqP-8uI",
-        categories: ["saúde", "esporte", "bodybuilder"]
-        },
-        {
-        podcastName: "flow",
-        episode: "RUBENS BARRICHELLO - Flow #339",
-        videoId: "4KDGTdiOV4I",
-        categories: ["esporte", "corrida"]
-        },
-    ]))
+    res.end(JSON.stringify(content))
 }
---
+++ projeto-app-podcast/src/services/list-episodes-services.ts
@@ -0,0 +1,21 @@
+
+
+
+
+export const serviceListEpisodes = async ()=>{
+    const data = [
+        {
+        podcastName: "flow",
+        episode: "CBUM - Flow #319",
+        videoId: "5DaLlqP-8uI",
+        categories: ["saúde", "esporte", "bodybuilder"]
+        },
+        {
+        podcastName: "flow",
+        episode: "RUBENS BARRICHELLO - Flow #339",
+        videoId: "4KDGTdiOV4I",
+        categories: ["esporte", "corrida"]
+        },
+    ]
+    return data
+}
