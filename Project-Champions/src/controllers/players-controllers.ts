9d14d49579837a54419e92cccf84d1e04527cbfd
NicoleGaia <nicgaia01@gmail.com>
2025-03-25 12:05:36 -0300

    create camada utils

--- Project-Champions/src/controllers/players-controllers.ts
+++ Project-Champions/src/controllers/players-controllers.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express"
 import { getPlayerService } from "../services/player-services"
+import { OK } from "../utils/http-helper"
 
 export const getPlayer = async (req: Request, res: Response) => {
 
     const data = getPlayerService()
-    res.status(200).json(data)
+    const response = await OK(data)
+
+    res.status(response.statusCode).json(response.body)
 }
---
+++ Project-Champions/src/utils/http-helper.ts
@@ -0,0 +1,11 @@
+interface HttpResponse {
+    statusCode: number
+    body: any
+}
+
+export const OK = async (data:any): Promise<HttpResponse> => {
+    return {
+        statusCode: 200,
+        body: data
+    }
+}
