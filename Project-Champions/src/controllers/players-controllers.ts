660d7c2000982bd47af93b8268c35070a8a7c181
NicoleGaia <nicgaia01@gmail.com>
2025-03-25 11:59:32 -0300

    create services

--- Project-Champions/src/controllers/players-controllers.ts
+++ Project-Champions/src/controllers/players-controllers.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express"
+import { getPlayerService } from "../services/player-services"
 
-export const getPlayer = (req: Request, res: Response) => {
-    res.status(200).json({player: "Ronaldo"})
+export const getPlayer = async (req: Request, res: Response) => {
+
+    const data = getPlayerService()
+    res.status(200).json(data)
 }
