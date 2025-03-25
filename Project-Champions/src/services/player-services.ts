d1a84c1fde07ef3a7f0a14fb9458daeb145da446
NicoleGaia <nicgaia01@gmail.com>
2025-03-25 14:35:41 -0300

    create repository

--- Project-Champions/src/services/player-services.ts
+++ Project-Champions/src/services/player-services.ts
@@ -1,10 +1,11 @@
+import { findAllPlayers } from "../repositories/players-repository"
 import { noContent, OK } from "../utils/http-helper"
 
 export const getPlayerService = async () =>{
-    const data = { player: "ronaldo"}
+    const data = await findAllPlayers()
     let response = null
 
-    if(!data){
+    if(data){
        response = await OK(data)
     } else {
         response = await noContent()
